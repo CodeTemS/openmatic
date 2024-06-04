87c17841a026f9883cd8de2968e254a14ecbaa7a
CodeTemS <cristo.montoya1722@outlook.com>
2024-06-04 13:14:46 -0600

    agregardo salarios

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useState } from 'react';
-import ODataStore from 'devextreme/data/odata/store';
+import {IntlProvider, FormattedMessage} from 'react-intl';
 import DataGrid, {
   Column,
   DataGridTypes,
@@ -10,6 +10,7 @@ import DataGrid, {
   SearchPanel,
 } from 'devextreme-react/data-grid';
 import jsonData from './datos.json';
+import mensajesEs from './lang/es-MX.json';
 
 const pageSizes = [10, 25, 50, 100];
 
@@ -24,27 +25,34 @@ const App = () => {
   }, [collapsed]);
 
   return (
-    <DataGrid
-      dataSource={jsonData}
-      allowColumnReordering={true}
-      rowAlternationEnabled={true}
-      showBorders={true}
-      width="100%"
-      onContentReady={onContentReady}
-    >
-      <GroupPanel visible={true} />
-      <SearchPanel visible={true} highlightCaseSensitive={true} />
-      <Grouping autoExpandAll={false} />
+    
+      <div>
+      <IntlProvider locale='es-MX' messages={mensajesEs} >
+        <DataGrid
+        dataSource={jsonData}
+        allowColumnReordering={true}
+        rowAlternationEnabled={true}
+        showBorders={true}
+        width="100%"
+        onContentReady={onContentReady}
+      >
+        <GroupPanel visible={true} />
+        <SearchPanel visible={true} highlightCaseSensitive={true} />
+        <Grouping autoExpandAll={true} />
 
-      <Column dataField="Futbolistas" groupIndex={0} />
-      
-      <Column dataField="name" dataType="string" />
-      <Column dataField="teams" dataType="string" />
-      <Column dataField="age" dataType="number" />
+        <Column dataField="Futbolistas" groupIndex={0} />
+        
+        <Column dataField="name" dataType="string" caption={mensajesEs['app.name']}/>
+        <Column dataField="teams" dataType="string" caption={mensajesEs['app.teams']}/>
+        <Column dataField="age" dataType="number" caption={mensajesEs['app.age']} />
+        <Column dataField="salary" dataType="number" format="currency" caption={mensajesEs['app.salary']} />
 
-      <Pager allowedPageSizes={pageSizes} showPageSizeSelector={true} />
-      <Paging defaultPageSize={10} />
-    </DataGrid>
+        <Pager allowedPageSizes={pageSizes} showPageSizeSelector={true} />
+        <Paging defaultPageSize={10} />
+      </DataGrid> 
+    </IntlProvider>
+      </div>
+    
   );
 };
 
