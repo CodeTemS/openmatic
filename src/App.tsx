d4ba32999e8197687f6c68c0c5bf922e4ba53af2
CodeTemS <cristo.montoya1722@outlook.com>
2024-06-04 14:08:34 -0600

    modificando los idiomas

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useCallback, useState } from 'react';
-import {IntlProvider, FormattedMessage} from 'react-intl';
+import React, { useCallback, useState, useContext } from 'react';
+import {IntlProvider} from 'react-intl';
 import DataGrid, {
   Column,
   DataGridTypes,
@@ -11,6 +11,7 @@ import DataGrid, {
 } from 'devextreme-react/data-grid';
 import jsonData from './datos.json';
 import mensajesEs from './lang/es-MX.json';
+import mensajesEn from './lang/en-US.json';
 
 const pageSizes = [10, 25, 50, 100];
 
@@ -24,33 +25,40 @@ const App = () => {
     }
   }, [collapsed]);
 
+  var mensajes = mensajesEs;
+
+
   return (
     
       <div>
-      <IntlProvider locale='es-MX' messages={mensajesEs} >
-        <DataGrid
-        dataSource={jsonData}
-        allowColumnReordering={true}
-        rowAlternationEnabled={true}
-        showBorders={true}
-        width="100%"
-        onContentReady={onContentReady}
-      >
-        <GroupPanel visible={true} />
-        <SearchPanel visible={true} highlightCaseSensitive={true} />
-        <Grouping autoExpandAll={true} />
+      <IntlProvider locale='es-MX' messages={mensajes}>
+      <div>
+          <button>Español</button>
+          <button>Ingles</button>
+        </div>
+          <DataGrid
+            dataSource={jsonData}
+            allowColumnReordering={true}
+            rowAlternationEnabled={true}
+            showBorders={true}
+            width="100%"
+            onContentReady={onContentReady}
+          >
+            <GroupPanel visible={true} />
+            <SearchPanel visible={true} highlightCaseSensitive={true} />
+            <Grouping autoExpandAll={true} />
 
-        <Column dataField="Futbolistas" groupIndex={0} />
-        
-        <Column dataField="name" dataType="string" caption={mensajesEs['app.name']}/>
-        <Column dataField="teams" dataType="string" caption={mensajesEs['app.teams']}/>
-        <Column dataField="age" dataType="number" caption={mensajesEs['app.age']} />
-        <Column dataField="salary" dataType="number" format="currency" caption={mensajesEs['app.salary']} />
+            <Column dataField="Futbolistas" groupIndex={0} />
+            
+            <Column dataField="name" dataType="string" caption={mensajes['app.name']}/>
+            <Column dataField="teams" dataType="string" caption={mensajes['app.teams']}/>
+            <Column dataField="age" dataType="number" caption={mensajes['app.age']} />
+            <Column dataField="salary" dataType="number" format="currency" caption={mensajes['app.salary']} />
 
-        <Pager allowedPageSizes={pageSizes} showPageSizeSelector={true} />
-        <Paging defaultPageSize={10} />
-      </DataGrid> 
-    </IntlProvider>
+            <Pager allowedPageSizes={pageSizes} showPageSizeSelector={true} />
+            <Paging defaultPageSize={10} />
+        </DataGrid> 
+      </IntlProvider>
       </div>
     
   );
