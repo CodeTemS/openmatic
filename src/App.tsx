9a9af3b9ebdc0809be0fd988e94b35f66286f530
CodeTemS <cristo.montoya1722@outlook.com>
2024-06-04 12:35:38 -0600

    agregando futbolistas

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,9 @@ import DataGrid, {
   Paging,
   SearchPanel,
 } from 'devextreme-react/data-grid';
+import jsonData from './datos.json';
 
 const pageSizes = [10, 25, 50, 100];
-
-const dataSourceOptions = {
-  store: new ODataStore({
-    version: 2,
-    url: 'https://js.devexpress.com/Demos/SalesViewer/odata/DaySaleDtoes',
-    key: 'Id',
-    beforeSend(request) {
-      const year = new Date().getFullYear() - 1;
-      request.params.startDate = `${year}-05-10`;
-      request.params.endDate = `${year}-5-15`;
-    },
-  }),
-};
 
 const App = () => {
   const [collapsed, setCollapsed] = useState(true);
@@ -37,7 +25,7 @@ const App = () => {
 
   return (
     <DataGrid
-      dataSource={dataSourceOptions}
+      dataSource={jsonData}
       allowColumnReordering={true}
       rowAlternationEnabled={true}
       showBorders={true}
@@ -48,28 +36,11 @@ const App = () => {
       <SearchPanel visible={true} highlightCaseSensitive={true} />
       <Grouping autoExpandAll={false} />
 
-      <Column dataField="Product" groupIndex={0} />
-      <Column
-        dataField="Amount"
-        caption="Sale Amount"
-        dataType="number"
-        format="currency"
-        alignment="right"
-      />
-      <Column
-        dataField="Discount"
-        caption="Discount %"
-        dataType="number"
-        format="percent"
-        alignment="right"
-        allowGrouping={false}
-        cssClass="bullet"
-      />
-      <Column dataField="SaleDate" dataType="date" />
-      <Column dataField="Region" dataType="string" />
-      <Column dataField="Sector" dataType="string" />
-      <Column dataField="Channel" dataType="string" />
-      <Column dataField="Customer" dataType="string" width={150} />
+      <Column dataField="Futbolistas" groupIndex={0} />
+      
+      <Column dataField="name" dataType="string" />
+      <Column dataField="teams" dataType="string" />
+      <Column dataField="age" dataType="number" />
 
       <Pager allowedPageSizes={pageSizes} showPageSizeSelector={true} />
       <Paging defaultPageSize={10} />
